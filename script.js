7fdad00a35fa41e266e723668ab964092a771d6e
pceolato <pedroceolato2002@gmail.com>
2022-01-14 15:18:10 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Modal = {
+    openModal() {
+        document.getElementById('modal')
+    .classList.add('active')
+    },
+    closeModal() {
+        document.getElementById('modal')
+        .classList.remove('active')
+        } 
+    }
+
+
+document.getElementById('cadastrarCliente')
+    .addEventListener('click', Modal.openModal)
+
+document.getElementById('modalClose')
+    .addEventListener('click', Modal.closeModal)
+
+document.getElementById('cancelar')
+    .addEventListener('click', Modal.closeModal)
+
+

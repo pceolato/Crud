9d1f3a1ce1ed9217fbd14bdc9dc7879fce5fbeed
pceolato <pedroceolato2002@gmail.com>
2022-01-24 11:57:52 -0300

    Alteração

--- script.js
+++ script.js
@@ -11,7 +11,22 @@ const Modal = {
         } 
     }
 
-    
+const tempClient = {
+    nome:"Pedrinho",
+    email:"pc@gmail.com",
+    celular:"1198766543",
+    cidade:"São Paulo"
+}
+// Crud - Create read update delete
+
+const createClient = (client) => {
+    const db_client = JSON.parse(localStorage.getItem('db_client'))
+    console.log(db_client)
+    db_client.push(client)
+    localStorage.setItem("db_client", JSON.stringify(db_client))
+}
+
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', Modal.openModal)
 
